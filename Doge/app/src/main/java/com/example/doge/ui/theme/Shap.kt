317a759f747a6b8b47880029947a4fdb64e95ce2
NeerAtHorizone <neerajsharma198118131@gmail.com>
2025-03-26 16:22:54 +0530

    show dogs info with a beautiful interface

---
+++ Doge/app/src/main/java/com/example/doge/ui/theme/Shap.kt
@@ -0,0 +1,10 @@
+package com.example.doge.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(50.dp),
+    medium = RoundedCornerShape(bottomStart = 32.dp, topEnd = 32.dp),
+)
